0c5418e5878ce8d2ddb252aa462f5ac2e548bfe5
RebecaCamilo <rebecacamilo3@gmail.com>
2024-11-11 22:08:50 -0300

    feat: method to check upper fields in edit page

--- client/cypress/pages/editPage.ts
+++ client/cypress/pages/editPage.ts
@@ -22,9 +22,155 @@ class EditPage {
         passwordRequiredErrorMessage: 'Password is required',
         invalidEmailOrPasswordErrorMessage: 'Invalid email or password',
         fansField: '[data-cy="fans"]',
-        savesField: '[data-cy="saves"]'
+        savesField: '[data-cy="saves"]',
+        priceField: '[data-cy="price"]',
+        powersField: '[data-cy="powers"]', //list div > ul > li
+        // heroCard: '[data-cy="hero-card"]',
+        // searchField: 'input[placeholder="Search Amazon"]',
+        // searchButton: "#nav-search-submit-button",
+        // listOfProducts: "div .s-result-list > div",
+        // singleProduct: '[data-cy="title-recipe"] > h2 > a > span'
+        // 
+        // 
+        pencilButton: '[data-cy="pencil"]',
+        heroNameTitleEditPage: '[data-cy="name"]',
+        
       };
     }
+
+
+    
+    clickInEditButton() {
+      cy.get(this.selectorsList().pencilButton).first().click();
+    }
+
+    checkIfInEditHeroPage() {
+      cy.get('[data-cy="name"]').should("exist");
+    }
+    
+    checkIfLoginButtonIsVisible() {
+      cy.contains("button", this.selectorsList().loginButtonText).should("be.visible");
+    }
+
+    checkIfLogoutButtonIsVisible() {
+      cy.contains("button", this.selectorsList().logoutButtonText).should("be.visible");
+    }
+
+    clickInLogo() {
+      cy.get(this.selectorsList().cyHeroesLogo).click();
+    }
+
+    clickInLoginButton() { 
+      cy.contains("button", this.selectorsList().loginButtonText).click();
+    }
+
+    clickInLogoutButton() {
+      cy.contains("button", this.selectorsList().logoutButtonText).click();
+    }
+
+    clickInSignInButton() {
+      cy.contains("button", this.selectorsList().signInButtonText).click();
+    }
+
+    typeInEmailLoginField(email: string) {
+      cy.get(this.selectorsList().loginEmailField).type(email);
+    }
+
+    typeInPasswordLoginField(pass: string) {
+      cy.get(this.selectorsList().loginPasswordField).type(pass);
+    }
+
+    login(email : string, pass : string) {
+      this.clickInLoginButton();
+
+      this.typeInEmailLoginField(email);
+      this.typeInPasswordLoginField(pass);
+      this.clickInSignInButton();
+    }
+
+
+
+
+    // checkTheNumberofSavesWhenHireFirstHero() {
+      // cy.get(this.selectorsList().savesField)
+    //     .first()
+    //     .invoke("text")
+    //     .then((savesNumber) => {
+    //       let initialSavesNumber = parseInt(savesNumber);
+
+    //       // this.clickInMoneyButtonOfFirstCard();
+    //       cy.contains("button", "Yes").click();
+
+    //       cy.get(this.selectorsList().savesField)
+    //         .first()
+    //         .invoke("text")
+    //         .then((updatedSavesNumber) => {
+    //           initialSavesNumber += 1;
+    //           expect(initialSavesNumber).to.eq(parseInt(updatedSavesNumber)
+    //           );
+    //         });
+    //     });
+    // }
+
+
+    checkTheFieldsInEditPage() {
+      let fansNumber: any;
+      let savesNumber: any;
+      let priceNumber: any;
+
+      // Localiza o card do produto específico
+      cy.get('[data-cy="hero-card"]')
+        .first()
+        .within(() => {
+          cy.get(this.selectorsList().fansField).invoke('text').then((text) => {
+            fansNumber = text;
+            console.log('Valor do produto no card:', fansNumber);
+          });
+
+          cy.get(this.selectorsList().savesField).invoke('text').then((text) => {
+            savesNumber = text;
+            console.log('Valor do produto no card:', savesNumber);
+          });
+
+          cy.get(this.selectorsList().priceField).invoke('text').then((text) => {
+            priceNumber = text;
+            console.log('Valor do produto no card:', priceNumber);
+          });
+  
+          // Clica no botão de editar dentro do card
+          this.clickInEditButton();
+        });
+  
+      cy.then(() => {  // Usando cy.then para garantir que Cypress espere a atribuição de savesNumber
+        cy.get(this.selectorsList().fansField)
+          .should('be.visible') // Garante que o campo esteja visível
+          .invoke('text') // Captura o texto do elemento
+          .should('equal', fansNumber) // Verifica se o texto está correto
+          .then(() => {
+            console.log('Valor do produto na página de edição:', fansNumber);
+          });
+
+          cy.get(this.selectorsList().savesField)
+          .should('be.visible') // Garante que o campo esteja visível
+          .invoke('text') // Captura o texto do elemento
+          .should('equal', savesNumber) // Verifica se o texto está correto
+          .then(() => {
+            cy.log('Valor do produto na página de edição:', savesNumber);
+            console.log('Valor do produto na página de edição:', savesNumber);
+          });
+
+          cy.get(this.selectorsList().priceField)
+          .should('be.visible') // Garante que o campo esteja visível
+          .invoke('text') // Captura o texto do elemento
+          .should('equal', priceNumber) // Verifica se o texto está correto
+          .then(() => {
+            cy.log('Valor do produto na página de edição:', priceNumber);
+            console.log('Valor do produto na página de edição:', priceNumber);
+          });
+      });
+
+    }    
+    
 
      
   }
