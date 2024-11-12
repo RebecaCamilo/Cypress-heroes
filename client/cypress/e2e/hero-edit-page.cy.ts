cfbd4077b7e48d17c19eb0d0342c02139fc33800
RebecaCamilo <rebecacamilo3@gmail.com>
2024-11-11 22:09:24 -0300

    feat: add method to check the upper fields

--- client/cypress/e2e/hero-edit-page.cy.ts
+++ client/cypress/e2e/hero-edit-page.cy.ts
@@ -8,12 +8,22 @@ const editPage = new EditPage()
 describe("hero-edit-page", () => {
     beforeEach(() => {
         cy.visit("/");
-        editPage.login(userData.admUserSuccess.user, userData.admUserSuccess.pass);
-        editPage.clickInEditButton();
+        editPage.login(userData.admUserSuccess.user, userData.admUserSuccess.pass);        
     })
 
     it("Logged admin user can access edit page", () => {
+        editPage.clickInEditButton();
         editPage.checkIfInEditHeroPage();
     });
+
+    //verificar se os dados no card do heroi sao os mesmos que na página de edição
+    it("Verify the hero data in edit page", () => {
+        editPage.checkTheFieldsInEditPage();
+        
+    })
+
+    //verificar se nenhum dos campos da pagina de edição está vazio
+
+    //verificar se após fazer alterações no heroi é possivel vê-la na tela home
 
 });
